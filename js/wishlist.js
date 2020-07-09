4fca6334e259bd0733cea86335ac73896d1b02d5
Luiza <luiza.lazareanu@simplica.com>
2020-07-09 19:58:27 +0300

    sign in/sign up form validation

--- js/wishlist.js
+++ js/wishlist.js
@@ -19,48 +19,102 @@ function loadProductsFromJson() {
     request.open('GET', requestURL);
 
     request.onload = function () {
-        const books = JSON.parse(request.responseText);
-        createContentFromJson(books);
+        var books = JSON.parse(request.responseText);
+        var selectedBook = JSON.parse(localStorage.getItem('bookForWishlist'));
+        // localStorage.removeItem('bookForWishlist');
+        books.push(selectedBook);
+        // createContentFromJson(books);
+        wishlisted(selectedBook);
     };
     request.send();
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     loadProductsFromJson();
-// });
-
-document.getElementById('json-btn').addEventListener('click', function(){
+document.addEventListener("DOMContentLoaded", function () {
     loadProductsFromJson();
 });
 
-function createContentFromJson(books){
-    var output="<div class='item' id='item'>";
+// document.getElementById('json-btn').addEventListener('click', function () {
+//     loadProductsFromJson();
+// });
 
-    for (var i in books) {
+//
+// function createContentFromJson(books) {
+//     var output = "<div class='item' id='item'>";
+//
+//     for (var i in books) {
+//         // if (books[i].id === 101) {
+//         output += "<div id='item" + i + "'>" + "<p>" + books[i].title + "</p> " + "<p>" + books[i].author + "</p>" + "<p>" + books[i].price + "</p>" + "</div>";
+//         // }
+//     }
+//     output += "</div>";
+//     document.getElementById("showItems").innerHTML = output;
+//
+//     for (var j in books) {
+//         var img = document.createElement("img");
+//         img.src = books[j].img;
+//         // document.getElementById('item').appendChild(img);
+//         var abc = 'item' + j;
+//         var parent = document.getElementById(abc);
+//         parent.insertBefore(img, parent.firstChild);
+//     }
+//
+//     for (var k in books) {
+//         var btn = document.createElement("button");
+//         btn.className = 'btn_class';
+//         btn.textContent = 'Add to cart';
+//         var item = 'item' + k;
+//         document.getElementById(item).appendChild(btn);
+//
+//         var span = document.createElement("span");
+//         span.className = 'remove_item';
+//         var item2 = 'item' + k;
+//         document.getElementById(item2).appendChild(span);
+//     }
+// }
 
-        // if (books[i].title === "This Must Be the Place") {
-            output += "<div id='item "+i+ "'>" +"<p>" + books[i].title + "</p> " + "<p>" + books[i].author + "</p>" + "<p>" + books[i].price + "</p>"+"</div>";
+function addToWishlist(books) {
+    //get book object dinamic not static
+    var wishlistBook = {
+        "id": 103,
+        "img": "./images/book_covers/boys.jpg",
+        "title": "The Nickel Boys: Pulitzer Prize",
+        "author": "Colson Whitehead",
+        "price": "6,98",
+        "category": "fiction"
+    };
+    localStorage.setItem("bookForWishlist", JSON.stringify(wishlistBook));
+    window.location.href = 'http://localhost:8080/test/web/wishlist.html';
 
-        // }
+    // var wishlistBook;
+    // for (var z in books) {
+    //     if (books[z].id === 103) {
+    //         selectedBook = books[z];
+    //         console.log(selectedBook);
+    //     }
+    // }
+    // localStorage.setItem("bookForWishlist", JSON.stringify(wishlistBook));
+    // window.location.href = 'http://localhost:8080/test/web/wishlist.html';
+}
 
-    }
-    output+="</div>";
-    document.getElementById("showTable").innerHTML=output;
+function wishlisted(selectedBook) {
+    var output2 = "<div class='item' id='item'>";
+    output2 += "<div id='item0'>" + "<p>" + selectedBook.title + "</p> " + "<p>" + selectedBook.author + "</p>" + "<p>" + selectedBook.price + "</p>" + "</div>";
+    output2 += "</div>";
+    document.getElementById("showItems").innerHTML = output2;
 
-    for(var j in books) {
-        var img = document.createElement("img");
-        img.src = books[j].img;
-        // document.getElementById('item').appendChild(img);
-        var abc = 'item ' + j;
-        var parent = document.getElementById(abc);
-        parent.insertBefore(img,parent.firstChild);
-    }
+    var img = document.createElement("img");
+    img.src = selectedBook.img;
+    var parent = document.getElementById('item0');
+    parent.insertBefore(img, parent.firstChild);
 
-    for(var k in books) {
-        var btn = document.createElement("button");
-        btn.className = 'btn_class';
-        btn.textContent = 'Add to cart';
-        var asd = 'item ' + k;
-        document.getElementById(asd).appendChild(btn);
-    }
+
+    var btn = document.createElement("button");
+    btn.className = 'btn_class';
+    btn.textContent = 'Add to cart';
+    document.getElementById('item0').appendChild(btn);
+
+    var span = document.createElement("span");
+    span.className = 'remove_item';
+    document.getElementById('item0').appendChild(span);
+
 }
